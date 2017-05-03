d865020b227a57d281ab37d1bff44ce13c721b2c
Piotr Gąsiorowski <gasiorowski.piotr@windowslive.com>
2017-05-03 23:25:50 +0200

    Added a basic service worker to cache static assets

--- src/index.js
+++ src/index.js
@@ -6,6 +6,15 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 
 injectTapEventPlugin();
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      console.log('ServiceWorker registration failed: ', err);
+    });
+  });
+}
 
 ReactDOM.render(
   <App />,
